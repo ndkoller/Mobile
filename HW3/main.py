3be2c1311a2b3ab2ebdf328ddee4973b9150ef8a
ndkoller <nicholas.koller@ymail.com>
2017-05-06 16:59:23 -0500

    variable

--- HW3/main.py
+++ HW3/main.py
@@ -37,9 +37,9 @@ class MainPage(webapp2.RequestHandler):
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
-        LOGIN_URI = LOGIN_URI+'?'+'response_type=code'+'client_id='+CLIENT_ID+'client_secret='+CLIENT_SECRET+'redirect_uri='+REDIRECT_URI+'scope=email'+'state=MyBigSecret123'+'access_type=offline'
-        self.response.write(LOGIN_URI)
-        urlfetch.fetch(LOGIN_URI,method=urlfetch.GET)
+        login = LOGIN_URI+'?'+'response_type=code'+'client_id='+CLIENT_ID+'client_secret='+CLIENT_SECRET+'redirect_uri='+REDIRECT_URI+'scope=email'+'state=MyBigSecret123'+'access_type=offline'
+        self.response.write(login)
+        urlfetch.fetch(login,method=urlfetch.GET)
         
 class OauthHandler(webapp2.RequestHandler):
     def get(self):
